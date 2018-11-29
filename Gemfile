daa60c91a69a60ae9e4a6fca1091439b4fa68f44
Jan <mail@jezekjan.com>
2018-11-29 11:41:57 +0100

    added rack-cors

--- Gemfile
+++ Gemfile
@@ -30,7 +30,7 @@ gem 'webpacker'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-# gem 'rack-cors'
+gem 'rack-cors', require: 'rack/cors'
 
 group :development, :test do
   gem 'rspec-rails'
--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,13 @@ module RailsApiReactBoilerplate
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Rack-Cors implementation
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
